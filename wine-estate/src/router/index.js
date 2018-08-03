62bb759e6cb2ce5a5dfa133d7b0e82f5a5e34ffa
zyzz <2808232184@qq.com>
2018-08-03 22:24:58 +0800

    l

--- wine-estate/src/router/index.js
+++ wine-estate/src/router/index.js
@@ -1,7 +1,15 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
+import goodList from '@/components/goodList/goodList'
+import goodSearch from '@/components/goodSearch/goodSearch'
+import goodClass from '@/components/goodClass/goodClass'
+import goodMain from '@/components/goodMain/goodMain'
+import goodDetail from '@/components/goodDetail/goodDetail'
+import Mint from 'mint-ui'
 
+import 'mint-ui/lib/style.css'
+Vue.use(Mint);
 Vue.use(Router)
 
 export default new Router({
@@ -10,6 +18,31 @@ export default new Router({
       path: '/',
       name: 'HelloWorld',
       component: HelloWorld
+    },
+    {
+      path: '/goodList',
+      name: 'goodList',
+      component: goodList
+    },
+    {
+      path: '/goodSearch',
+      name: 'goodSearch',
+      component: goodSearch
+    },
+    {
+      path: '/goodClass',
+      name: 'goodClass',
+      component: goodClass
+    },
+    {
+      path: '/goodMain',
+      name: 'goodMain',
+      component: goodMain
+    },
+    {
+      path: '/goodDetail',
+      name: 'goodDetail',
+      component: goodDetail
     }
   ]
 })
