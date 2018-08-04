4626057aa37bb339cfe854a5b1674f3a262379b3
sloth <492400398@qq.com>
2018-08-04 18:36:27 +0800

    add

--- wine-estate/src/components/orderPay/orderPay.vue
+++ wine-estate/src/components/orderPay/orderPay.vue
@@ -1,70 +1,95 @@
 <template>
-<div class="main">
-  <div id="top"></div>
-  <div id="header" >
-    <div id="img"><img src="../../assets/icon2.png"></div>
-    <span>确认付款</span>
+  <div class="main">
+    <div id="top"></div>
+    <div class="back">
+      <img src="../../assets/payback.jpg" height="532" width="750"/>
+    </div>
+    <section class="head clearfix" >
+      <img src="../../assets/icon22.jpg" width="39" height="52">
+      <span>我的订单</span>
+    </section>
+    <section class="pay-way">
+      <div class="price">&yen<span></span></div>
+      <div id="message">账号</div>
+      <ul>
+        <li>
+          <img src="../../assets/beforeicon.jpg" height="42" width="42"/>
+          &nbsp &nbsp 支付宝付款</li>
+        <li>
+          <img src="../../assets/beforeicon.jpg" height="42" width="42"/>
+          &nbsp &nbsp 微信付款</li>
+      </ul>
+    </section>
+    <section class="pay">
+      <input type="button" value="立即付款">
+    </section>
   </div>
-  <div id="back"><img src="../../assets/payback.jpg" height="532" width="750"/></div>
-  <div id="content"></div>
-  <div id="footer">
-    <input type="button" value="立即付款">
-  </div>
-</div>
 </template>
 
 <script>
-    export default {
-        name: "orderPay"
-    }
+  export default {
+    name: "orderPay"
+  }
 </script>
 
 <style scoped>
   @import url(../../style/common.css);
   .main{
     margin: 0 auto;
-   }
+    width: 750px;
+  }
   #top{
     height: 40px;
     width:750px;
     background:#bb3437;
-    margin: 0 auto;
   }
-  #back{
-    width: 750px;
-    margin: 0 auto;}
-  #header{
-    background: #bb3437;
+  .back{
+    height: 532px;
+  }
+  .head{
+    background: #f6f6f6;
     width:750px;
     margin: 0 auto;
-    padding-top: 18px;
+    padding: 18px 0px 18px 17px;
     position:relative;
-    padding-bottom: 18px;}
-  #img{
-    width: 39px;
-    height: 52px;
-    margin-left: 17px;}
-  #header span{
+  }
+  img{
+    float: left}
+  .head span{
     font-size: 37px;
-    color: #fff;position:absolute;
+    position:absolute;
     top:0;left:0;right:0;bottom:0;
-    margin-top: 18px;}
-  #content{
+    margin-top: 18px}
+  .pay-way{
     border-top: 1px solid #dcdcdc;
-    margin: 0 auto;
     width: 750px;
     background: #f6f6f6;
     padding: 57px 0 117px 27px;
+    text-align: left;
   }
+  .price{
+    font-size: 50px;
+    margin:0 0 68px 238px;
+  }
+  #message{
+    font-size: 30px;
+    margin-bottom: 36px;
+    color: #676767;
+    border-bottom: #eaeaea solid 1px;
 
-
-#footer{
-  width: 750px;
-  padding: 0 32px 29px 32px;
-  margin: 0 auto;
-  background:#f6f6f6 ;
-}
-  #footer input{
+  }
+  ul li{
+    font-size: 30px;
+    margin: 30px 0 30px;
+    color: #676767;
+    border-bottom: #eaeaea solid 1px;
+  }
+  .pay{
+    width: 750px;
+    padding: 0 32px 29px 32px;
+    background:#f6f6f6 ;
+  }
+  .pay input{
     background: #1b8de3;
     height: 84px;
     border-radius:12% ;
