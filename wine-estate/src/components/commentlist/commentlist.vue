8a0caaa5402caf69e4666ce864c57ab03c01d088
sloth <492400398@qq.com>
2018-08-05 22:26:45 +0800

    add

--- wine-estate/src/components/commentlist/commentlist.vue
+++ wine-estate/src/components/commentlist/commentlist.vue
@@ -1,5 +1,11 @@
 <template>
-
+  <div class="main">
+    <div class="top"></div>
+    <section class="head clearfix" >
+      <img src="../../assets/icon2.png" width="39" height="52">
+      <span>我的订单</span>
+    </section>
+  </div>
 </template>
 
 <script>
@@ -9,5 +15,21 @@
 </script>
 
 <style scoped>
-
+  @import url(../../style/common.css);
+  .main{margin: 0 auto;}
+  .top{
+    height: 40px;
+    width:750px;
+    background:#bb3437;
+    margin: 0 auto;
+  }
+  .head{
+    background: #bb3437;
+    width:750px;
+    margin: 0 auto;
+    padding: 18px 0px 18px 17px;
+    position:relative;
+  }
+  img{
+    float: left}
 </style>
